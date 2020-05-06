e33db434118f1904ae6d2d1e37c5c4cf000ae086
lohanlhn <lohan.massote@gmail.com>
2020-05-06 11:07:31 -0300

    entidades criadas

---
+++ Entities/Chamado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Chamado
+    {
+        public int id { get; set; }
+        public string descricao { get; set; }
+        public DateTime dateAbertura { get; set; }
+        public double custo { get; set; }
+
+    }
+}
