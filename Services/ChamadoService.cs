ac4ea831a2e3d5547169cc0c5a8ceecb81ee027a
lohanlhn <lohan.massote@gmail.com>
2020-05-12 11:37:53 -0300

    Service Iniciado

---
+++ Services/ChamadoService.cs
@@ -0,0 +1,20 @@
+﻿using Entities;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    class ChamadoService
+    {
+        //TODO terminar comando
+        public List<Chamado> Listar()
+        {
+            MySqlCommand cmd = new MySqlCommand("SELECT ");
+        }
+
+    }
+}
---
+++ Services/ConexaoBanco.cs
@@ -0,0 +1,95 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Services
+{
+    /// <summary>
+    /// Classe para conexão com o banco de dados
+    /// </summary>
+    public class ConexaoBanco
+    {
+        //TODO mudar database
+        #region Atributos da classe
+       
+        /// <summary>
+        /// Objeto de conexão com o MySQL
+        /// </summary>
+        protected MySqlConnection mySqlConnection;
+
+        #endregion
+
+        #region Metodos da classe
+
+        /// <summary>
+        /// Metodo construtor da classe
+        /// </summary>
+        public ConexaoBanco()
+        {
+            mySqlConnection = new MySqlConnection("Server=localhost;Database=dbteste;Uid=root;Pwd=1234");
+        }
+
+        /// <summary>
+        /// Abre a conexao com o banco de dados
+        /// </summary>
+        public void AbrirConexao()
+        {
+
+            if (mySqlConnection.State != ConnectionState.Open)
+                mySqlConnection.Open();
+
+        }
+
+        /// <summary>
+        /// Fecha a conexao com o banco de dados
+        /// </summary>
+        public void FecharConexao()
+        {
+
+            if (mySqlConnection.State != ConnectionState.Closed)
+                mySqlConnection.Close();
+
+        }
+
+        /// <summary>
+        /// Metodo para pesquisar dados no banco
+        /// </summary>
+        /// <param name="cmd">Comando SQL</param>
+        /// <returns>Dados recuperados do banco</returns>
+        public MySqlDataReader Pesquisar(MySqlCommand cmd)
+        {
+            try
+            {
+
+                cmd.Connection = mySqlConnection;
+                return cmd.ExecuteReader();
+
+            }
+            catch
+            {
+                FecharConexao();
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Metodo para executar comando SQL
+        /// </summary>
+        /// <param name="cmd">Comando SQL</param>
+        public void Executar(MySqlCommand cmd)
+        {
+            try
+            {
+                cmd.Connection = mySqlConnection;
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                FecharConexao();
+                throw;
+            }
+        }
+
+        #endregion
+
+    }
+}
